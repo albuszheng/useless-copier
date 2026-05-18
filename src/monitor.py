3aed66affda87333a6e9aa7ba400a04351186da8
albuszheng <albuszheng@hotmail.com>
2026-05-18 13:09:09 -0700

    watchdog testing for linux file change event

--- src/monitor.py
+++ src/monitor.py
@@ -1,19 +1,19 @@
 import time
 from logger import logger
 
-from watchdog.events import DirModifiedEvent, FileSystemEventHandler
+from watchdog.events import DirModifiedEvent, FileSystemEventHandler, FileModifiedEvent
 from watchdog.observers import Observer
 
-PATH = '.'
+PATH = '/home/albsun/Documents/testing/from/'
 
 class DirModifiedHandler(FileSystemEventHandler):
-    def on_modified(self, event: DirModifiedEvent) -> None:
+    def on_modified(self, event: DirModifiedEvent | FileModifiedEvent) -> None:
         print(f"Directory changed, event: {event}")
         logger.info(f"Directory changed, event: {event}")
 
-event_handler = DirModifiedEvent()
+event_handler = DirModifiedHandler()
 observer = Observer()
-observer.schedule(event_handler=event_handler, path=PATH, recursive=True)
+observer.schedule(event_handler, path=PATH, recursive=True)
 observer.start()
 try:
     while True:
